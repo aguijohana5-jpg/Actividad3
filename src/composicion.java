231b07e329a91ad1be9a44bf6c7b2c1e33c3f993
aguijohana5-jpg <aguijohana5@gmail.com>
2026-03-16 20:21:37 -0600

    Update composicion.java

--- src/composicion.java
+++ src/composicion.java
@@ -1,42 +1,29 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class composicion {
-    private String titulo;
-    private int duracionMinutos;
-    private int duracionSegundos;
-    private String generoMusical;
-    private String fechaRegistro;
-    private String fechaEstreno;
-    private List<Solicitud> listaSolicitudes;
-    public composicion(String titulo,int duracionMinutos, int duracionSegundos, String generoMusical, String fechaRegistro, String fechaEstreno) {
+   String titulo;
+    int duracionMinutos;
+    int duracionSegundos;
+    String generoMusical;
+    String fechaRegistro;
+    String fechaEstreno;
 
-        this.titulo = titulo;
-        this.duracionMinutos = duracionMinutos;
-        this.duracionSegundos = duracionSegundos;
-        this.generoMusical = generoMusical;
-        this.fechaRegistro = fechaRegistro;
-        this.fechaEstreno = fechaEstreno;
-        this.listaSolicitudes = new ArrayList<>();
+    ArrayList<Solicitud> listaSolicitudes = new ArrayList<>();
+
+    public composicion(String t, int m, int s, String g, String fr, String fe) {
+        titulo = t;
+        duracionMinutos = m;
+        duracionSegundos = s;
+        generoMusical = g;
+        fechaRegistro = fr;
+        fechaEstreno = fe;
     }
+
     public void agregarSolicitud(Solicitud s) {
-        if (s.validarFechas()) {
-            listaSolicitudes.add(s);
-        }
-    }
-    public void eliminarSolicitud(Solicitud s) {
-        listaSolicitudes.remove(s);
-    }
-    public int obtenerDuracionTotal() {
-        return (duracionMinutos * 60) + duracionSegundos;
-    }
-    @Override
-    public String toString() {
-        return "Composicion: " + titulo + ", Duracion total (seg): " + obtenerDuracionTotal();
+        listaSolicitudes.add(s);
     }
 
-    private class Solicitud {
-        public boolean validarFechas() {
-        }
+    public int obtenerDuracionTotal() {
+        return duracionMinutos * 60 + duracionSegundos;
     }
 }
