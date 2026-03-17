40236de19a6c39cbf19326891abcaeccbc0286b8
aguijohana5-jpg <aguijohana5@gmail.com>
2026-03-16 20:24:35 -0600

    Update Solicitud.java

--- src/Solicitud.java
+++ src/Solicitud.java
@@ -1,22 +1,14 @@
 public class Solicitud {
 
-    private String nombreInterprete;
-    private String fechaSolicitud;
-    private String fechaAutorizacion;
-    private String companiaGrabacion;
+    String nombreInterprete;
+    String fechaSolicitud;
+    String fechaAutorizacion;
+    String companiaGrabacion;
 
-    public Solicitud(String nombreInterprete, String fechaSolicitud, String fechaAutorizacion, String companiaGrabacion) {
-        this.nombreInterprete = nombreInterprete;
-        this.fechaSolicitud = fechaSolicitud;
-        this.fechaAutorizacion = fechaAutorizacion;
-        this.companiaGrabacion = companiaGrabacion;
-    }
-    public boolean validarFechas() {
-        return fechaAutorizacion.compareTo(fechaSolicitud) >=0;
-    }
-
-    @Override
-    public String toString() {
-        return "Solicitud de: " + nombreInterprete + ", Compañia: " + companiaGrabacion;
+    public Solicitud(String n, String fs, String fa, String c){
+        nombreInterprete = n;
+        fechaSolicitud = fs;
+        fechaAutorizacion = fa;
+        companiaGrabacion = c;
     }
 }
